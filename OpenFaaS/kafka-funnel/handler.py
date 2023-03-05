76334640f12572a703bff2bb1ad4c932d1b74e7c
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2023-03-05 15:34:17 +0100

    new(openfaas): DNN inference

---
+++ OpenFaaS/kafka-funnel/handler.py
@@ -0,0 +1,26 @@
+from kafka3 import KafkaProducer
+from functools import lru_cache
+from json import loads
+from os import environ
+
+@lru_cache(maxsize=None)
+def get_producer():
+    args = loads(environ["FUNNEL_PRODUCER"])
+    return KafkaProducer(**args)
+
+@lru_cache(maxsize=None)
+def get_producer_options():
+    return loads(environ.get("FUNNEL_SEND", default="{}"))
+
+def handle(event, context):
+    payload = loads(event.body)
+    producer = get_producer()
+    kwargs = get_producer_options()
+
+    producer.send(
+        payload["topic"],
+        str(payload["value"]).encode(),
+        **kwargs
+    ).get(timeout=30)
+    
+    return {"statusCode": 200}

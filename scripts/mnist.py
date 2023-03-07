104cad34b42f37b1b63e50e34b62d4ba7b88ee37
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2023-03-07 10:05:08 +0100

    ref(scripts): Allow mnist selection of dataset

--- scripts/mnist.py
+++ scripts/mnist.py
@@ -3,33 +3,35 @@ from kafka3 import KafkaProducer
 
 from functools import lru_cache
 from typing import List
+from json import dumps
+
+from lib import NumpyEncoder
+
 
 @lru_cache(maxsize=None)
 def mnist():
     from tensorflow import keras
     return keras.datasets.mnist.load_data()
 
-def payload(start: int, end: int):
-    from lib import NumpyEncoder
-    from json import dumps
-    
-    (x_train, y_train), (x_test, y_test) = mnist()
-    value = x_test[start:end]
+def payload(start: int, end: int, dataset = 0):    
+    # (x_train, y_train), (x_test, y_test) = mnist()
+    test = mnist()[1][dataset]
+    value = test[start:end]
     return dumps({"value": value}, cls=NumpyEncoder)
 
 app = Typer()
 
 @app.command()
-def json(blocksize: int = 1, offset: int = 0):
-    p = payload(offset, offset + blocksize)
+def json(blocksize: int = 1, offset: int = 0, dataset: int = 0):
+    p = payload(offset, offset + blocksize, dataset=dataset)
     print(p)
 
 @app.command()
-def kafka(topic: str, brokers: List[str], blocksize: int = 1, offset: int = 0, messages: int = 5):
+def kafka(topic: str, brokers: List[str], blocksize: int = 1, offset: int = 0, messages: int = 5, dataset: int = 0):
     producer = KafkaProducer(bootstrap_servers=brokers)
 
     for i in range(messages):
-        p = payload(offset + i * blocksize, offset + (1 + i * blocksize))
+        p = payload(offset + i * blocksize, offset + (1 + i * blocksize), dataset=dataset)
         producer.send(topic, p.encode())
 
     producer.flush()

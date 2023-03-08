ad26881e0654dcdc8a9263da2c2ba6bf0e706ba6
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2023-03-08 10:07:01 +0100

    fix(script): Extract dataset

--- scripts/mnist.py
+++ scripts/mnist.py
@@ -13,25 +13,24 @@ def mnist():
     from tensorflow import keras
     return keras.datasets.mnist.load_data()
 
-def payload(start: int, end: int, dataset = 0):    
-    # (x_train, y_train), (x_test, y_test) = mnist()
-    test = mnist()[1][dataset]
+def payload(start: int, end: int):    
+    (x_train, y_train), (test, y_test) = mnist()
     value = test[start:end]
     return dumps({"value": value}, cls=NumpyEncoder)
 
 app = Typer()
 
 @app.command()
-def json(blocksize: int = 1, offset: int = 0, dataset: int = 0):
-    p = payload(offset, offset + blocksize, dataset=dataset)
+def json(blocksize: int = 1, offset: int = 0):
+    p = payload(offset, offset + blocksize)
     print(p)
 
 @app.command()
-def kafka(topic: str, brokers: List[str], blocksize: int = 1, offset: int = 0, messages: int = 5, dataset: int = 0):
+def kafka(topic: str, brokers: List[str], blocksize: int = 1, offset: int = 0, messages: int = 5):
     producer = KafkaProducer(bootstrap_servers=brokers)
 
     for i in range(messages):
-        p = payload(offset + i * blocksize, offset + (1 + i * blocksize), dataset=dataset)
+        p = payload(offset + i * blocksize, offset + (1 + i * blocksize))
         producer.send(topic, p.encode())
 
     producer.flush()

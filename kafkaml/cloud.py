5e8bc40d0fdd707861e70da2c2a20294815e7aae
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2023-03-10 12:12:57 +0100

    new(kafkaml): DDNN model

--- kafkaml/cloud.py
+++ kafkaml/cloud.py
@@ -1,8 +1,3 @@
-# From https://github.com/ertis-research/kafka-ml/blob/4542ab5dab6e3d03706ac020aedf2bd9ee4bbec0/README.md?plain=1#L279
-
-cloud_input = keras.Input(shape=64, name='cloud_input')
-x = tf.keras.layers.Dense(64, activation=tf.nn.relu, name='relu1')(cloud_input)
-x = tf.keras.layers.Dense(128, activation=tf.nn.relu, name='relu2')(x)
-x = tf.keras.layers.Dropout(0.2)(x)
-cloud_output = tf.keras.layers.Dense(10, activation=tf.nn.softmax, name='cloud_output')(x)
+cloud_input = keras.Input(shape=16, name='cloud_input')
+cloud_output = tf.keras.layers.Dense(10, activation=tf.nn.softmax, name='cloud_output')(cloud_input)
 cloud_model = keras.Model(inputs=cloud_input, outputs=[cloud_output], name='cloud_model')
